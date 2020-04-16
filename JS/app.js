9169bb5b1631c24ff0b2ea08195f3f4b8fa2175b
paulmrest <paulmrest@gmail.com>
2020-04-16 09:03:37 -0700

    Removed test code from JS

--- JS/app.js
+++ JS/app.js
@@ -1,14 +1,6 @@
 function daysSincePeopleOnMoon() {
     var dateLastOnMoon = new Date('12/14/1972');
     var timeDiffMicroSeconds = Math.abs(new Date() - dateLastOnMoon);
-    //TEST
-    console.log('timeDiffMicroSeconds ' + timeDiffMicroSeconds);
-    //TEST
-
-    /*
-    return Math.ceil(timeDiffMicroSeconds / (1000 * 24 * 60 * 60))
-    */
-
     document.getElementById('dayssincemoon').innerHTML = Math.ceil(timeDiffMicroSeconds / (1000 * 24 * 60 * 60));
 }
 
@@ -17,24 +9,13 @@ function calculateCost() {
     //get value from text box
     var weightFromTextBox = document.getElementById('enterweight').value;
 
-    //TEST
-    console.log('weightFromTextBox ' + weightFromTextBox);
-    //TEST
-
     //determine if weight is in pounds or kilograms
     //if it is in pounds, convert to kilograms
     var weightKilograms = weightInKilograms(weightFromTextBox);
 
-    //TEST
-    console.log('weightKilograms ' + weightKilograms);
-    //TEST
-
     const lunarCostUSDPerKilogram = 2000000; //2 million USD per kilogram
     var totalCost = lunarCostUSDPerKilogram * weightKilograms
 
-    //TEST
-    console.log('It would cost USD $' + totalCost.toFixed(2) + ' to send you and your luggage to the Moon.')
-    //TEST
     document.getElementById('calculatedcost').innerHTML = 'It would cost USD $' + totalCost.toFixed(2) + ' to send you and your luggage to the Moon.';
 }
 
@@ -47,9 +28,6 @@ function weightInKilograms(weight) {
         if (weightButtons[i].checked)
         {
             var weightUnit = weightButtons[i].value;
-            //TEST
-            console.log('weightUnit ' + weightUnit);
-            //TEST
             if (weightUnit == 'kilograms')
             {
                 return weight;
@@ -77,8 +55,5 @@ function cheeseGuesser() {
         'Epoisses'
     ]
     var psuedoRandomChoice = Math.floor(Math.random() * Math.floor(cheeses.length));
-    //TEST
-    console.log('psuedoRandomChoice ' + psuedoRandomChoice);
-    //TEST
     document.getElementById('cheesetype').innerHTML = cheeses[psuedoRandomChoice];
 }
